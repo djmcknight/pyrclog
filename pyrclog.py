9c37ac99684acd39f6dd8245dc8b005105730671
djmcknight <djmcknight@users.noreply.github.com>
2014-02-06 04:13:40 -0500

    added comments for clarity

--- pyrclog.py
+++ pyrclog.py
@@ -14,13 +14,18 @@ channels = ['#test1', '#test2', '#test3']
 now = datetime.now()
 logbase = '/some/log/dir/for/base'
 
+#Function for returning datetime month-day-year.log for the filename of the logfile - this creates the Logrotation becuase
+#when it's a new day - it will automatially write to this new file.
 def filename():
     return str(datetime.now().month) + '-' + str(datetime.now().day) + '-' + str(datetime.now().year) + '.' + 'log'
 
+#Time format for when we receive an alert in [hour:minute] format
 def timeformat():
     return str(datetime.now().hour) + ':' + str(datetime.now().minute)
 
-def startup(): #Checks to see if the dirs you're trying to log to are accessible, if not exit script
+#You need to have the logbase + dir of the channel you're going to be watching for. So - if you need to add a new 
+#channel to watching in 'channels' - you need to mkdir new channel in the logbase dir.
+def startup(): 
     try:
         for channel in channels:
             channel = channel.split('#')[1]
@@ -29,32 +34,36 @@ def startup(): #Checks to see if the dirs you're trying to log to are accessible
         return False
         sys.exit()
     return True
-startup()
+startup() #calls Startup to check the channels log dir
 
+#opens the socket
 ircsoc = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+#connnects to irc server asiged to 'server', and 'port'
 ircsoc.connect(( server, port))
+#sleep for one second
 time.sleep(1)
 ircsoc.send('NICK pyrclog\r\n' )
 time.sleep(1)
 ircsoc.send('USER pyrclog pyrclog pyrclog :Python IRClogger Maintained by Derek McKnight\r\n' )
 time.sleep(1)
 
-
+#Chceks to see if startup function is true. This is an infinite loop
 while startup():
     for channel in channels:
-        ircsoc.send('JOIN %s\r\n' % channel)
+        ircsoc.send('JOIN %s\r\n' % channel) #Joins the channels
     ircmsg = ircsoc.recv(2048)
     ircmsg=ircmsg.strip('\n\r')
-    if ircmsg.find('PRIVMSG') != -1:
-        nick=ircmsg.split('!')[0][1:]
-        channel=ircmsg.split(' PRIVMSG' )[-1].split(' :')[0]
-        new_channel=channel.split('#')[1]
-        msg=ircmsg.split('PRIVMSG')[-1].split(' :')[1]
+    if ircmsg.find('PRIVMSG') != -1: #If Statement to look for messages from users in IRC channels
+        nick=ircmsg.split('!')[0][1:]                        #Message formatting
+        channel=ircmsg.split(' PRIVMSG' )[-1].split(' :')[0] #More message formatting
+        new_channel=channel.split('#')[1]                    #Even more message formatting
+        msg=ircmsg.split('PRIVMSG')[-1].split(' :')[1]       #Lots mor message formatting
+        #The 'with open' opens the new file with the current date, as seen in the filename function - this creates the
+        #log rotation ability because this will only write to files of the current date - when it becomes a new day
+        #you get a new file. This also makes sure that you don't have to explicitly close the file after each call.
         with open(logbase + new_channel + '/' + filename() , 'a') as logfile:
             logfile.write('[%s]%s <%s>: %s\n' % (timeformat(),channel,nick,msg))
-        #print '[%s]%s <%s>: %s' % (hrse,channel, nick, msg)
-
-
-
+    #Responds to the IRC PING responses. If you don't aswer the PING with PONG and the numeral in the PING request
+    #you will eventually be timed out of the server
     if ircmsg.find( 'PING' ) != -1:
         ircsoc.send('PONG ' + ircmsg.split()[1] + '\r\n')
