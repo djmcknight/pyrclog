98d72c8180b18f8dfdf9a908a26ea5eac74857c2
djmcknight <djmcknight@users.noreply.github.com>
2014-02-14 19:49:16 -0500

    first commit

---
+++ pyrclog_monitor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import os, commands, string, subprocess, sys, smtplib
+
+
+#--------------------Email Info-----------------
+SERVER = 'localhost'
+FROM = 'you@example.com'
+TO = ['recepiant@example.com']
+SUBJECT = 'The IRC logger is not running'
+TEXT = '''The IRC LOGGER is no longer running'''
+
+
+message = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (FROM, ",".join(TO), SUBJECT, TEXT)
+server = smtplib.SMTP(SERVER)
+#------ End of Email info--------------
+
+#--------Program INFO--------------
+tmpfile = '/tmp/pyrclog.pid'
+if os.path.isfile(tmpfile):
+    pidfile = open(tmpfile, 'rw')
+    pid = pidfile.readlines()
+    pidfile.close()
+
+#Getting status of pid in pidfile
+output = commands.getoutput("ps -ef | grep -v grep | grep " + pid[0])
+
+proginfo = string.split(output)
+
+if len(proginfo) > 0:
+    if proginfo[8] == 'pyrclog.py':
+        sys.exit()
+else:
+    server.sendmail(FROM, TO, message)
+    server.quit()
+    sys.exit()
