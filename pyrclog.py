f2f3c3323cfeb1580cd23829fe7918d181af6122
djmcknight <djmcknight@users.noreply.github.com>
2014-02-14 19:45:31 -0500

    added pid file, fixed timeformat

--- pyrclog.py
+++ pyrclog.py
@@ -1,36 +1,51 @@
-#!/usr/bin/pyhon
+#!/usr/bin/python
 import socket
 from datetime import datetime
 import os
 import sys
 import time
 
+#pid file info
+pid = str(os.getpid())
+pidfile = '/tmp/pyrclog.pid'
+if os.path.isfile(pidfile):
+    print '%s already exsists, exiting' % pidfile
+    sys.exit()
+else:
+    with open(pidfile,'w') as pid_file:
+        pid_file.write(pid)
+
 #connection info
 server = 'irchost'
 port = 6667
 channels = ['#test1', '#test2', '#test3']
+nick = 'pyrclog.py'
+
 
 #formatting stuff
 now = datetime.now()
-logbase = '/some/log/dir/for/base'
+logbase = '/home/pyrclog/logs/'
 
 #Function for returning datetime month-day-year.log for the filename of the logfile - this creates the Logrotation becuase
 #when it's a new day - it will automatially write to this new file.
 def filename():
-    return str(datetime.now().month) + '-' + str(datetime.now().day) + '-' + str(datetime.now().year) + '.' + 'log'
+    return str(datetime.now().month) + '-' + str(datetime.now().day) + '-' + str(datetime.now().year) + '.' + 'txt'
 
 #Time format for when we receive an alert in [hour:minute] format
 def timeformat():
-    return str(datetime.now().hour) + ':' + str(datetime.now().minute)
+    tf = datetime.now()
+    toutput = tf.strftime('%H:%M')
+    return toutput
+    #return str(datetime.now().hour) + ':' + str(datetime.now().minute)
 
-#You need to have the logbase + dir of the channel you're going to be watching for. So - if you need to add a new 
+#You need to have the logbase + dir of the channel you're going to be watching for. So - if you need to add a new
 #channel to watching in 'channels' - you need to mkdir new channel in the logbase dir.
-def startup(): 
+def startup():
     try:
         for channel in channels:
             channel = channel.split('#')[1]
             os.listdir(logbase + channel) #Tries to list dir
-    except OSError: #If it can't, use the OSError it rasies
+    except OSError: #If it can't use the OSError it rasies
         return False #and return false
         sys.exit() #then exit
     return True #IF it works - return true.
@@ -40,8 +55,9 @@ startup() #calls Startup to check the channels log dir
 ircsoc = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 #connnects to irc server asiged to 'server', and 'port'
 ircsoc.connect(( server, port))
-time.sleep(1) #sleep for one second
-ircsoc.send('NICK pyrclog\r\n' )
+#sleep for one second
+time.sleep(1)
+ircsoc.send('NICK %s\r\n' % nick )
 time.sleep(1) #More Sleep
 ircsoc.send('USER pyrclog pyrclog pyrclog :Python IRClogger Maintained by Derek McKnight\r\n' )
 time.sleep(1) #bot is sleepy
@@ -63,8 +79,10 @@ while startup():
             #you get a new file. This also makes sure that you don't have to explicitly close the file after each call.
             with open(logbase + new_channel + '/' + filename() , 'a') as logfile:
                 logfile.write('[%s]%s <%s>: %s\n' % (timeformat(),channel,nick,msg))
-        
+
     #Responds to the IRC PING responses. If you don't aswer the PING with PONG and the numeral in the PING request
     #you will eventually be timed out of the server
     if ircmsg.find( 'PING' ) != -1:
         ircsoc.send('PONG ' + ircmsg.split()[1] + '\r\n')
+
+
